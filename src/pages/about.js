93ea4ccd8758562e3534a7fdab8eaa1ced4ba8d1
HugoVeraGarcia <hugo.vera.garcia@gmail.com>
2023-06-03 19:58:54 -0500

    first commit

---
+++ src/pages/about.js
@@ -0,0 +1,136 @@
+import AnimatedText from "@/components/AnimatedText";
+import Education from "@/components/Education";
+import Experience from "@/components/Experience";
+import Layout from "@/components/Layout";
+import Skills from "@/components/Skills";
+import TransitionEffect from "@/components/TransitionEffect";
+import { useInView, useMotionValue, useSpring } from "framer-motion";
+import Head from "next/head";
+import Image from "next/image";
+import React, { useEffect, useRef } from "react";
+import profilePic from "../../public/images/profile/developer-pic-2.jpg";
+
+const AnimatedNumbers = ({ value }) => {
+  const ref = useRef(null);
+
+  const motionValue = useMotionValue(0);
+  const springValue = useSpring(motionValue, { duration: 3000 });
+  const isInView = useInView(ref, { once: true });
+
+  useEffect(() => {
+    if (isInView) {
+      motionValue.set(value);
+    }
+  }, [isInView, value, motionValue]);
+
+  useEffect(() => {
+    springValue.on("change", (latest) => {
+      if (ref.current && latest.toFixed(0) <= value) {
+        ref.current.textContent = latest.toFixed(0);
+      }
+    });
+  }, [springValue, value]);
+
+  return <span ref={ref}></span>;
+};
+
+const about = () => {
+  return (
+    <>
+      <Head>
+      <title>Minimal Portfolio Built with Nextjs | About Page</title>
+        <meta name="description" content="Learn more about CodeBucks, a Next.js developer with a passion for 
+        creating innovative solutions. Discover tips for building a developer portfolio and insights on 
+        full-stack development, front-end development, and back-end development." />
+      </Head>
+      <TransitionEffect />
+      <main className="flex w-full flex-col items-center justify-center dark:text-light">
+        <Layout className="pt-16">
+          <AnimatedText
+            text="Passion Fuels Purpose!"
+            className="mb-16 !leading-tight lg:!text-7xl sm:!text-6xl xs:!text-4xl sm:mb-8"
+          />
+          <div className="grid w-full grid-cols-8 gap-16 sm:gap-8">
+            <div className="col-span-3 flex flex-col items-start justify-start xl:col-span-4 md:order-2 md:col-span-8">
+              <h2 className="mb-4 text-lg font-bold uppercase text-dark/75 dark:text-light/75">
+                Biography
+              </h2>
+              <p className="font-medium">
+              Hola, soy Roberto Villanueva, camarógrafo y reportero apasionado por capturar momentos significativos y contar historias impactantes a través de la imagen en movimiento. Con 20 años de experiencia en el campo, siempre busco nuevas formas de transmitir la verdad y la emoción a través de mis reportajes. Ya sea que esté documentando eventos, entrevistando a personas fascinantes o explorando lugares remotos, mi compromiso con la excelencia y la narración visual se refleja en cada proyecto en el que participo. Estoy emocionado de poder aportar mis habilidades y pasión a su próximo proyecto y ayudar a dar vida a su visión.
+              </p>
+
+              <p className="my-4 font-medium">
+              Creo que el arte de la narración va más allá de capturar imágenes llamativas: se trata de descubrir la verdad y crear experiencias auténticas y conmovedoras para el público. Como camarógrafo y reportero, mi enfoque va más allá de la estética visual, buscando resolver problemas y transmitir historias de manera intuitiva y emocionante.
+              </p>
+
+              <p className="font-medium">
+              Ya sea que esté reportando desde el terreno, filmando documentales o cubriendo eventos en vivo, traigo mi compromiso con la excelencia en la narración visual y el enfoque centrado en el público a cada proyecto en el que participo. Espero con entusiasmo la oportunidad de aportar mis habilidades y pasión a su próximo proyecto, ya sea como camarógrafo o reportero, y juntos crear contenido impactante y significativo.
+              </p>
+            </div>
+
+            <div
+              className="col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark
+bg-light p-8 dark:bg-dark dark:border-light xl:col-span-4 md:order-1 md:col-span-8
+"
+            >
+              <div className="absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark dark:bg-light" />
+              <Image
+                src={profilePic}
+                alt="Codebucks"
+                className="w-full h-auto rounded-2xl"
+                priority
+                sizes="(max-width: 768px) 100vw,
+              (max-width: 1200px) 50vw,
+              33vw"
+              />
+            </div>
+
+            <div className="col-span-2 flex flex-col items-end justify-between xl:col-span-8 xl:flex-row xl:items-center md:order-3">
+              <div className="flex flex-col items-end justify-center xl:items-center">
+                <span className="inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl">
+                  <AnimatedNumbers value={50} />+
+                </span>
+                <h2
+                  className="text-xl font-medium capitalize text-dark/75 dark:text-light/75 xl:text-center md:text-lg sm:text-base
+                xs:text-sm"
+                >
+                  satisfied clients
+                </h2>
+              </div>
+
+              <div className="flex flex-col items-end justify-center xl:items-center">
+                <span className="inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl">
+                  <AnimatedNumbers value={40} />+
+                </span>
+                <h2
+                  className="text-xl font-medium capitalize text-dark/75 dark:text-light/75 xl:text-center md:text-lg sm:text-base
+                xs:text-sm"
+                >
+                  projects completed
+                </h2>
+              </div>
+
+              <div className="flex flex-col items-end justify-center xl:items-center">
+                <span className="inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl">
+                  <AnimatedNumbers value={4} />+
+                </span>
+                <h2
+                  className="text-xl font-medium capitalize text-dark/75 dark:text-light/75 xl:text-center md:text-lg sm:text-base
+                xs:text-sm"
+                >
+                  years of experience
+                </h2>
+              </div>
+            </div>
+          </div>
+
+          <Skills />
+          <Experience />
+          <Education />
+        </Layout>
+      </main>
+    </>
+  );
+};
+
+export default about;
